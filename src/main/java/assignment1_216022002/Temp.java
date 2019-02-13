6f0bddf2aa0fd980d62dd7fa3c571b2821ed81b7
Matthew booysen <booysen31.matthew@gmail.com>
2019-02-13 20:28:11 +0200

    error message

---
+++ src/main/java/assignment1_216022002/Temp.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1_216022002;
+
+/**
+ *
+ * @author Matthew
+ */
+public class Temp {
+    
+    private String type;
+    private int value;
+
+    public Temp(String type, int value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    
+}
--- src/test/java/assignment1_216022002/Temperature_converterTest.java
+++ src/test/java/assignment1_216022002/Temperature_converterTest.java
@@ -1,4 +1,7 @@
 package assignment1_216022002;
+import static org.hamcrest.CoreMatchers.equalTo;
+import org.hamcrest.core.IsEqual;
+import org.junit.Assert;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,6 +12,13 @@ public class Temperature_converterTest {
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    
+    @Test
+    public void tempTest(){
+        Temp t = new Temp("f", 90);
+        Assert.assertThat(t.getType(), equalTo("f"));
+        Assert.assertThat(t.getValue(), equalTo(90));
     }
 
 }
